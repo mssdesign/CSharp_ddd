cc9c01829a8ac4cf9a373561b9f4f7ecc404526f
Matheus Soares <matheussilvasoares43@gmail.com>
2023-02-05 21:00:24 -0300

    Banco de dados

---
+++ EFCore_Introducao/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using EFCore_Introducao.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore_Introducao
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<Client> Client { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=.;Database=EFCore;Trusted_Connection=True");
+        }
+    }
+}
---
+++ EFCore_Introducao/Program.cs
@@ -0,0 +1,37 @@
+﻿using EFCore_Introducao.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore_Introducao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            AppDbContext db = new AppDbContext();
+
+            Client client = new Client()
+            {
+                Name = "Matheus",
+                Data_Nascimento = new DateTime(1998, 6, 20)
+            };
+
+            db.Client.Add(client);
+
+            Client client2 = new Client()
+            {
+                Name = "Pedro",
+                Data_Nascimento = new DateTime(1995, 5, 21)
+            };
+
+            db.Client.Add(client);
+
+            Console.ReadKey();
+        }
+    }
+}
