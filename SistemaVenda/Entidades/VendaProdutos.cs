1660a56690bd54cdd2535e6c4b5b529e3920157e
Matheus Soares <matheussilvasoares43@gmail.com>
2023-02-12 18:19:23 -0300

    entiti framework

---
+++ SistemaVenda/Entidades/VendaProdutos.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SistemaVenda.Entidades
+{
+    public class VendaProdutos
+    {
+        public int CodigoVenda { get; set; }
+        public int CodigoProduto { get; set; }
+        public double Quantidade { get; set; }
+        public decimal ValorUnitario { get; set; }
+        public decimal ValorTotal { get; set; }
+        public Produto Produto { get; set; }
+        public Venda Venda { get; set; }
+    }
+}
