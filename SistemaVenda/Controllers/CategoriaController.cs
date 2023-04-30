82b7cf65c616b1b7e5946d3dc762752fc77ff399
Matheus Soares <matheussilvasoares43@gmail.com>
2023-04-29 21:54:45 -0300

    Inclusão de categorias

---
+++ SistemaVenda/Controllers/CategoriaController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaVenda.DAL;
+using SistemaVenda.Entidades;
+using SistemaVenda.Models;
+
+namespace SistemaVenda.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        protected ApplicationDbContext mContext;
+
+        public CategoriaController(ApplicationDbContext context)
+        {
+            mContext = context; //injeção de dependência
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Categoria> lista = mContext.Categoria.ToList();
+            mContext.Dispose(); //Liberando memória
+            return View(lista);
+        }
+
+        [HttpGet]
+        public IActionResult Cadastro(int? id)
+        {
+            CategoriaViewModel viewModel = new CategoriaViewModel();
+            return View(viewModel);
+        }
+    }
+}
---
+++ SistemaVenda/Models/CategoriaViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SistemaVenda.Models
+{
+    public class CategoriaViewModel
+    {
+        public int? Codigo { get; set; }
+        public string Descricao { get; set; }
+    }
+}
