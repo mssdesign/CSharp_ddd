5beb9ada1634a215f1dba5a02f82a5d3ef8823d4
Matheus Soares <matheussilvasoares43@gmail.com>
2023-05-06 21:09:01 -0300

    Crud

--- SistemaVenda/Controllers/CategoriaController.cs
+++ SistemaVenda/Controllers/CategoriaController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SistemaVenda.DAL;
 using SistemaVenda.Entidades;
 using SistemaVenda.Models;
@@ -25,7 +26,58 @@ namespace SistemaVenda.Controllers
         public IActionResult Cadastro(int? id)
         {
             CategoriaViewModel viewModel = new CategoriaViewModel();
+
+            if (id != null)
+            {
+                var entidade = mContext.Categoria.Where(x => x.Codigo == id).FirstOrDefault();
+                viewModel.Codigo = entidade.Codigo;
+                viewModel.Descricao = entidade.Descricao;
+            }
+
             return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(CategoriaViewModel entidade)
+        {
+            if (ModelState.IsValid)
+            {
+                Categoria objCategoria = new Categoria()
+                {
+                    Codigo = entidade.Codigo,
+                    Descricao = entidade.Descricao
+                };
+
+                if (entidade.Codigo == null)
+                {
+                    mContext.Categoria.Add(objCategoria);
+                } 
+                else
+                {
+                    //Fazendo atualização de uma categoria
+                    mContext.Entry(objCategoria).State = EntityState.Modified;
+                }
+
+                mContext.SaveChanges();
+            }
+            else
+            {
+                return View(entidade);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Excluir(int id)
+        {
+            var ent = new Categoria()
+            {
+                Codigo = id
+            };
+            mContext.Attach(ent);
+            mContext.Remove(ent);
+            mContext.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
--- SistemaVenda/Models/CategoriaViewModel.cs
+++ SistemaVenda/Models/CategoriaViewModel.cs
@@ -1,8 +1,12 @@
-﻿namespace SistemaVenda.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaVenda.Models
 {
     public class CategoriaViewModel
     {
         public int? Codigo { get; set; }
+
+        [Required(ErrorMessage="Informe a descrição da categoria.")]
         public string Descricao { get; set; }
     }
 }
